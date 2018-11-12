38d72d7c768e3950c4880191a10ed626eef3f0dd
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-12 10:42:11 -0500

    part 2 done?; did stuff with github info? think i got it all?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const GITHUB_URL = "https://api.github.com/users/mhoelzer";
+
 class App extends Component {
-  handleClickMeButt = event => {
-    this.setState(prevState => {
-      console.log("hellojolnl,")
-    });
+  state = {
+    user: {},
+    active: false
   };
+
+  handleClickMeButt = event => {
+    // this.setState(prevState => {
+    //   console.log("hellojolnl,")
+    // });
+    fetch(GITHUB_URL)
+      .then(response => response.json())
+      .then(githubInfo => {
+        this.setState({user: githubInfo})
+        console.log({githubInfo})
+        // console.log({user: githubInfo}); // same as above
+      })
+      .catch(err => console.log(`${err} error error error`))
+  };
+
   render() {
     return (
       <div>
