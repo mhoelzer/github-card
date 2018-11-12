511cd81f3aa5a15a49998cd56aab922ce7fafb1c
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-12 11:47:38 -0500

    part 3 should be done?

--- src/App.js
+++ src/App.js
@@ -10,23 +10,38 @@ class App extends Component {
   };
 
   handleClickMeButt = event => {
-    // this.setState(prevState => {
-    //   console.log("hellojolnl,")
-    // });
     fetch(GITHUB_URL)
       .then(response => response.json())
       .then(githubInfo => {
-        this.setState({user: githubInfo})
-        console.log({githubInfo})
+        this.setState({user: githubInfo});
+        console.log({githubInfo});
         // console.log({user: githubInfo}); // same as above
       })
-      .catch(err => console.log(`${err} error error error`))
+      .catch(err => console.log(`${err} error error error`));
+    this.setState(prevState => ({
+      // console.log("hellojolnl,")
+      active: !prevState.active
+    }));
   };
 
   render() {
     return (
       <div>
         <button onClick={this.handleClickMeButt}>Click Me</button>
+        <div>
+          {this.state.active ? 
+            <div>
+              <img src={this.state.user.avatar_url}/>
+              <p>Name: {this.state.user.name}</p>
+              <p>Username: {this.state.user.login}</p>
+              <p>Portfolio: 
+                <a href={this.state.user.blog} target="_blank">{this.state.user.blog}</a>
+              </p>
+              <p>Public Repositories: {this.state.user.public_repos}</p>
+            </div>
+          : null}
+          {/* could also do this.state.active && ... and leave off the : null */}
+        </div>
       </div>
     );
   }
