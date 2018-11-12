278a326cb5c1c62080b77ecd8a19012f4cd7a098
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-12 16:29:43 -0500

    part 4 should be done

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
       <div className="main">
         <br />
         <Container>
-          <a className="orange btn-floating btn-large waves-effect waves-red" onClick={this.handleClickMeButt}><i class="material-icons">music_note</i></a>
+          <Button floating large className="orange btn-large waves-effect waves-red" onClick={this.handleClickMeButt}>:)</Button>
           {this.state.active ?
             <div>
               {/* <Button floating large className="orange btn-large waves-effect waves-red" onClick={this.handleClickMeButt} icon="clear"></Button>  --> if do this, put the above where null is */}
