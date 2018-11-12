87cb4112f877acb56a0bf33025076086445ba8f7
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-12 10:14:42 -0500

    part 1 done; added button

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClickMeButt = event => {
+    this.setState(prevState => {
+      console.log("hellojolnl,")
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.handleClickMeButt}>Click Me</button>
       </div>
     );
   }
